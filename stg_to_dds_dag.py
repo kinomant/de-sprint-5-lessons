9e2a5c8c0ac18ecfe31401a3d4d7fbc3b42d693a
kinomant <kinomant@users.noreply.github.com>
2022-08-28 17:36:15 +0300

    Update stg_to_dds_dag.py

--- stg_to_dds_dag.py
+++ stg_to_dds_dag.py
@@ -42,30 +42,6 @@ def restaurants_extract ():
             max_update_ts = record[2]
             PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.srv_wf_settings', rows=[['restaurants', max_update_ts]], target_fields=target_fields_srv_wf_settings)
     
-def ts_extract ():
-    records = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_records(
-    """
-    with t as (
-    select object_id, object_value::json->'final_status' as status,
-    update_ts::TIMESTAMP,
-    EXTRACT(YEAR FROM update_ts)::integer as year,
-    EXTRACT(MONTH FROM update_ts)::integer as month,
-    EXTRACT(DAY FROM update_ts)::integer as day,
-    EXTRACT(HOUR FROM update_ts)::integer as hour,
-    EXTRACT(MINUTE FROM update_ts)::integer as minute,
-    EXTRACT(SECOND FROM update_ts)::integer as second
-    from stg.ordersystem_orders)
-    select update_ts, year, month, day, make_time (hour, minute, second) as time, make_date (year, month, day) as date from t
-    where update_ts > (select coalesce(max(workflow_settings), '1980-01-01')::timestamp(6) from dds.srv_wf_settings where workflow_key = 'timestamps')
-    """)
-    max_update_ts = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_first("""select coalesce(max(workflow_settings), '1980-01-01')::timestamp(6) from dds.srv_wf_settings where workflow_key = 'timestamps'""")[0]
-    for record in records:
-        values = [[record[0], record[1], record[2], record[3], record[4], record[5]]]
-        target_fields = ['ts', 'year', 'month', 'day', 'time', 'date']
-        PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.dm_timestamps', rows=values, target_fields=target_fields)
-        if record[0] > max_update_ts:
-            max_update_ts = record[0]
-            PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.srv_wf_settings', rows=[['timestamps', max_update_ts]], target_fields=target_fields_srv_wf_settings)
 
 def products_extract ():
     records = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_records(
@@ -90,41 +66,103 @@ def products_extract ():
             max_update_ts = record[6]
             PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.srv_wf_settings', rows=[['products', max_update_ts]], target_fields=target_fields_srv_wf_settings)
 
+def ts_extract ():
+    records = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_pandas_df(
+    """
+    with t as (
+    select object_id, object_value::json->'final_status' as status,
+    (object_value::json->'date'->>'$date')::timestamp(6) as update_ts,
+    EXTRACT(YEAR FROM update_ts)::integer as year,
+    EXTRACT(MONTH FROM update_ts)::integer as month,
+    EXTRACT(DAY FROM update_ts)::integer as day,
+    EXTRACT(HOUR FROM update_ts)::integer as hour,
+    EXTRACT(MINUTE FROM update_ts)::integer as minute,
+    EXTRACT(SECOND FROM update_ts)::integer as second
+    from stg.ordersystem_orders)
+    select update_ts, year, month, day, make_time (hour, minute, second) as time, make_date (year, month, day) as date from t
+    where update_ts > (select coalesce(max(workflow_settings), '1980-01-01')::timestamp(6) from dds.srv_wf_settings where workflow_key = 'timestamps')
+    """)
+    target_fields = ['ts', 'year', 'month', 'day', 'time', 'date']
+    PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.dm_timestamps', rows=records.values, target_fields=target_fields)
+    max_update_ts = records.max()[0]
+    if len(records)>0:
+        PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.srv_wf_settings', rows=[['timestamps', max_update_ts]], target_fields=target_fields_srv_wf_settings)
 
 def orders_extract ():
-    records = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_records(
+    records = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_pandas_df(
     """
-    with tmp as (select object_id as order_key, 
-    (object_value::json->'date'->>'$date')::timestamp(6) as upd_ts,
-    object_value::json->'restaurant'->'id'->>'$oid' as rest_id,
-    object_value::json->'user'->'id'->>'$oid' as u_id,
-    object_value::json->>'final_status' as status,
-    update_ts
-            from stg.ordersystem_orders)
+    with tmp as 
+        (select object_id as order_key, 
+        (object_value::json->'date'->>'$date')::timestamp(6) as upd_ts,
+        object_value::json->'restaurant'->'id'->>'$oid' as rest_id,
+        object_value::json->'user'->'id'->>'$oid' as u_id,
+        object_value::json->>'final_status' as status,
+        update_ts
+        from stg.ordersystem_orders)
     select du.id as user_id, dr.id as restaurant_id, dt.id as timestamp_id, t.order_key, t.status, t.update_ts from tmp t
-    join dds.dm_restaurants dr 
-    on t.rest_id=dr.restaurant_id 
-    join dds.dm_users du 
-    on t.u_id=du.user_id 
-    join dds.dm_timestamps dt 
-    on t.upd_ts=dt.ts
+        left join dds.dm_restaurants dr 
+        on t.rest_id=dr.restaurant_id 
+        left join dds.dm_users du 
+        on t.u_id=du.user_id 
+        left join dds.dm_timestamps dt 
+        on t.upd_ts=dt.ts
     where t.upd_ts>dr.active_from and t.upd_ts<dr.active_to and t.upd_ts>dr.active_from and t.upd_ts<dr.active_to and 
     t.update_ts > (select coalesce(max(workflow_settings), '1980-01-01')::timestamp(6) from dds.srv_wf_settings where workflow_key = 'orders')
     """)
-    max_update_ts = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_first("""select coalesce(max(workflow_settings), '1980-01-01')::timestamp(6) from dds.srv_wf_settings where workflow_key = 'orders'""")[0]
-    for record in records:
-        values = [[record[0], record[1], record[2], record[3], record[4]]]
-        target_fields = ['user_id', 'restaurant_id', 'timestamp_id', 'order_key', 'order_status']
-        PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.dm_orders', rows=values, target_fields=target_fields)
-        if record[5] > max_update_ts:
-            max_update_ts = record[5]
-            PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.srv_wf_settings', rows=[['orders', max_update_ts]], commit_every=100, target_fields=target_fields_srv_wf_settings)
+    max_update_ts = records.max()[5]
+    records.drop(records.columns[5], axis=1, inplace=True)
+    target_fields = ['user_id', 'restaurant_id', 'timestamp_id', 'order_key', 'order_status']
+    PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.dm_orders', rows=records.values, target_fields=target_fields)
+    if len(records)>0:
+        PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.srv_wf_settings', rows=[['orders', max_update_ts]], target_fields=target_fields_srv_wf_settings)
+
+def generate_fct_product_sales ():
+    records = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_pandas_df(
+    """
+    SELECT
+        dp.id as product_id,
+        ddo.id as order_id,
+        q."count",
+        q.price,
+        q.total_sum,
+        q.bonus_payment,
+        q.bonus_grant,
+        event_ts
+    FROM
+    (
+    SELECT 
+        json_array_elements(sbe.event_value::json->'product_payments')->>'product_id' AS product_id,	
+        sbe.event_value::json->>'order_id' AS order_id,
+        (json_array_elements(sbe.event_value::json->'product_payments')->>'quantity')::numeric AS "count",
+        (json_array_elements(sbe.event_value::json->'product_payments')->>'price')::numeric AS price,
+        (json_array_elements(sbe.event_value::json->'product_payments')->>'product_cost')::numeric AS total_sum,
+        (json_array_elements(sbe.event_value::json->'product_payments')->>'bonus_payment')::numeric AS bonus_payment,
+        (json_array_elements(sbe.event_value::json->'product_payments')->>'bonus_grant')::numeric AS bonus_grant,
+        sbe.event_ts::timestamp AS event_ts
+    FROM stg.bonussystem_events sbe
+    WHERE sbe.event_type='bonus_transaction'
+    ) AS q
+    LEFT JOIN dds.dm_products dp ON dp.product_id = q.product_id AND dp.active_to > q.event_ts AND dp.active_from < q.event_ts
+    LEFT JOIN dds.dm_orders ddo ON ddo.order_key = q.order_id
+    WHERE q.event_ts > 
+    (
+    SELECT 
+    coalesce(max(workflow_settings), '1980-01-01')::timestamp(6) 
+    from dds.srv_wf_settings 
+    where workflow_key ='fct_product_sales' )
+    """)
+    max_update_ts = records.max()[7]
+    records.drop(records.columns[7], axis=1, inplace=True)
+    target_fields = ['product_id', 'order_id', 'count', 'price', 'total_sum', 'bonus_payment', 'bonus_grant']
+    PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.fct_product_sales', rows=records.values, target_fields=target_fields)
+    if len(records)>0:
+        PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('dds.srv_wf_settings', rows=[['fct_product_sales', max_update_ts]], target_fields=target_fields_srv_wf_settings)
 
 
 with DAG(dag_id='stg_to_dds', start_date=pendulum.datetime(2022, 5, 5, tz="UTC"), schedule_interval='0/15 * * * *', catchup=False) as dag:
     t1 = PythonOperator(
     task_id='users_extract',
-    python_callable=users_extract
+    python_callable=users_extract 
     ) 
     t2 = PythonOperator(
     task_id='restaurants_extract',
@@ -142,5 +180,9 @@ with DAG(dag_id='stg_to_dds', start_date=pendulum.datetime(2022, 5, 5, tz="UTC")
     task_id='orders_extract',
     python_callable=orders_extract
     )
+    t6 = PythonOperator(
+    task_id='generate_fct_product_sales',
+    python_callable=generate_fct_product_sales
+    ) 
 
-t1 >> t2 >> t4 >> t3 >> t5
+t1 >> t2 >> t4 >> t3 >> t5 >> t6
